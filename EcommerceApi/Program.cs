41f71519e56747e0d9d8669a774e7dd2ae814866
edf abidjan <appimmo.abidjan@gmail.com>
2026-04-30 01:00:31 +0000

    disable auto migration

--- EcommerceApi/Program.cs
+++ EcommerceApi/Program.cs
@@ -220,7 +220,7 @@ using (var scope = app.Services.CreateScope())
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    //db.Database.Migrate();
 }
 
 app.UseSwagger();
